4b0f47948ad6c37df6bb352313875815ced532ec
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-28 22:37:03 +0800

    singleton implemented

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,4 +2,23 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
+    public  static  ThreadSafeSingleton instance = null;
+    private static final Object mutex = new Object();
+
+    private ThreadSafeSingleton() {}
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton result = instance;
+
+        if (result == null) {
+            synchronized (mutex){
+                result = instance;
+                if (result == null) {
+                    instance = result = new ThreadSafeSingleton();
+                }
+            }
+        }
+
+        return result;
+    }
 }
