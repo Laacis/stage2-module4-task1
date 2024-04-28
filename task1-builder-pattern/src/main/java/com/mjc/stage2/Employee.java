7148666f8c2a504caefce334bd7df878fdf30be1
Laacis <109668309+Laacis@users.noreply.github.com>
2024-04-28 21:47:54 +0800

    Build pattern implemented

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -69,6 +69,47 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        private Employee employee;
+
+        public EmployeeBuilder() {
+            this.employee = new Employee();
+        }
+
+        public EmployeeBuilder setName(String name) {
+            employee.setName(name);
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            employee.setLastName(lastName);
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            employee.setPosition(position);
+            return this;
+        }
+
+        public EmployeeBuilder setPhone(String phone) {
+            employee.setPhone(phone);
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            employee.setEmail(email);
+            return this;
+        }
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            employee.setCarNumber(carNumber);
+            return this;
+        }
+
+        public Employee build() throws Exception {
+            if (employee.getName() == null || employee.getLastName() == null || employee.getPosition() == null || employee.getEmail() == null || employee.getCarNumber() == null ) {
+                throw new Exception("All employee fields must be set before build.");
+            }
+
+            return  employee;
+        }
     }
 }
